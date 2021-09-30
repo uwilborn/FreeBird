0ad572b5b864dd176f55f52d9612c90a55181512
Ursula Wilborn <wilbornu@yahoo.com>
2021-09-29 21:41:49 -0400

    create models

---
+++ server/models/Parks.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const siteSchema = new Schema({
+  type: {type: String, required: true},
+  site_name: {type: String, required: true, unique: true},
+  site_location: {type: String, required: true, unique: true},
+  site_url: {type: String, required: true, unique: true},
+  site_address: 
+    { type: {type: String},
+      line1: {type: String},
+      line2: {type: String},
+      line3: {type: String},
+      city: {type: String},
+      statecode: {type: String},
+      postalcode: {type: String},
+    },
+description: {type: String, required: true, unique: true},
+});
+
+
+const Parks = model('Parks', siteSchema);
+
+module.exports = Parks;
