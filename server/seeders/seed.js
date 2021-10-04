afb2d874c7723f091a56f6c01a3fcca5e751a4e5
Ursula Wilborn <82011348+uwilborn@users.noreply.github.com>
2021-10-04 10:44:01 -0400

    Update seed.js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,14 @@
 const db = require('../config/connection');
 const { User, Park } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const siteSeeds = require('./userSeeds.json');
+const siteSeeds = require('./siteSeeds.json');
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
     await User.create(userSeeds);
+    await Park.deleteMany({});
+    await Park.create(siteSeeds);
   } catch (err) {
     console.error(err);
     process.exit(1);
