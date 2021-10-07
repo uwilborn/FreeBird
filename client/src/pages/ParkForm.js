7e25fdf398e8e47c4eec58a466fcbd15e9cc85a8
Ursula Wilborn <wilbornu@yahoo.com>
2021-10-06 22:41:08 -0400

    update form

---
+++ client/src/pages/ParkForm.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">
+      Type:
+    </label>
+    <input
+      type="email"
+      class="form-control"
+      id="exampleInputEmail1"
+      aria-describedby="emailHelp"
+    />
+    <div id="emailHelp" class="form-text">
+      We'll never share your email with anyone else.
+    </div>
+  </div>
+
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+    <label class="form-check-label" for="exampleCheck1">
+      Park
+    </label>
+  </div>
+
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+    <label class="form-check-label" for="exampleCheck1">
+      Monument
+    </label>
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">
+      Name:
+    </label>
+    <input type="password" class="form-control" id="exampleInputPassword1" />
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">
+      State:
+    </label>
+    <input type="password" class="form-control" id="exampleInputPassword1" />
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">
+      Website:
+    </label>
+    <input type="password" class="form-control" id="exampleInputPassword1" />
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">
+      Description
+    </label>
+    <input type="password" class="form-control" id="exampleInputPassword1" />
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">
+      Address
+    </label>
+    <input type="password" class="form-control" id="exampleInputPassword1" />
+  </div>
+
+  <button type="submit" class="btn btn-primary">
+    Submit
+  </button>
+</form>;
