f0374e3e7aa1c0709c0edd0c0c8af9e711ce564e
Ursula Wilborn <wilbornu@yahoo.com>
2021-10-08 13:11:05 -0400

    update form

--- client/src/pages/ParkForm.js
+++ client/src/pages/ParkForm.js
@@ -2,103 +2,67 @@ import React from "react";
 
 function ParkForm() {
   return (
-    <form>
-      <div className="mb-3">
-        <label for="exampleInputEmail1" className="form-label">
-          Type:
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
+    <div className="testbox">
+      <form action="/">
+        <div className="banner">
+          <h1>Virtual Trip Booking Form</h1>
         </div>
-      </div>
 
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" for="exampleCheck1">
-          Park
-        </label>
-      </div>
+        <div className="item">
+          <p>Category:</p>
+          {/* <input type="text" id="site_type" /> */}
+          <select>
+            <option value="">*Please select*</option>
+            <option value="1">National Park</option>
+            <option value="2">Monument</option>
+            <option value="3">Museum</option>
+          </select>
+        </div>
 
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" for="exampleCheck1">
-          Monument
-        </label>
-      </div>
+        <div className="item">
+          <label for="nameofsite" className="form-label">
+            Site Name:
+          </label>
+          <input type="nameofsite" className="form-control" id="site_name" />
+        </div>
 
-      <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          Name:
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
+        <div className="item">
+          <p>Description: </p>
+          <input type="text" className="form-control" id="description" />
+        </div>
 
-      <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          State:
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
+        <div className="item, city-item">
+          <p>Address:</p>
+          <input type="text" id="type" placeholder="Physical or Mailing" />
+          <input type="text" id="line1" placeholder="Street address" />
+          <input type="text" id="line2" placeholder="Street address line 2" />
+          <input type="text" id="line3" placeholder="Street address line 3" />
+          <div className="city-item">
+            <input type="text" id="city" placeholder="City" />
+            <input type="text" id="statecode" placeholder="State" />
+            <input
+              type="text"
+              id="postalcode"
+              placeholder="Postal / Zip code"
+            />
+            <input type="text" id="country" placeholder="Country" />
+          </div>
+        </div>
 
-      <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          Website:
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
+        <div className="item">
+          <p for="site_url" className="form-label">
+            Website:
+          </p>
+          <input type="text" className="form-control" id="site_url" />
+        </div>
 
-      <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          Description
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-
-      <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          Address
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+        <div class="btn-block">
+          <button type="submit" className="btn btn-primary" href="/">
+            SEND
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
