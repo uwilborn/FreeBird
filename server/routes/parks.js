44bfec8706bea17303b898e75534380645547240
Ursula Wilborn <wilbornu@yahoo.com>
2021-10-09 18:43:05 -0400

    updated routes'

--- server/routes/parks.js
+++ server/routes/parks.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 // const mongoose = require("mongoose");
 const Axios = require("axios");
-// const Park = require("../models/Park");
+const Park = require("../models/Park");
 
 router.get("/api/parks/results", (req, res) => {
   console.log("========API Call======");
